838479b8875535a81b47a625118b25ba898d5d88
Arsh Mansuri <arsh.amin.may.2006@gmail.com>
2026-07-18 19:23:19 +0530

    node list vs HTML Collection

--- day-1/script.js
+++ day-1/script.js
@@ -1,6 +1,12 @@
 // console.log(window);
 
-const h1=document.querySelector("h1");
-h1.textContent="hello i am changed through dom"
+// const h1=document.querySelector("h1");
+// h1.textContent="hello i am changed through dom"
+
+// const h1=document.querySelectorAll("h1") Static Node list 
+
+// const h1=document.getElementsByTagName("h1"); Live -HTML Collection
+
+
 
 console.log(h1);
