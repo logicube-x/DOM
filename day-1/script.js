a7362df12e1ff8bc437a02efe59892cbea1ab8da
Arsh Mansuri <arsh.amin.may.2006@gmail.com>
2026-07-20 22:58:27 +0530

    done revision

--- day-1/script.js
+++ day-1/script.js
@@ -7,6 +7,13 @@
 
 // const h1=document.getElementsByTagName("h1"); Live -HTML Collection
 
+// const h1=document.getElementById("sec")
 
+// we will always querySelector
 
+const h1=document.querySelector(".first")
+const box=document.querySelector("#box");
+
+box.style.padding="40px";
+box.style.backgroundColor="crimson"
 console.log(h1);
---
+++ day-2/script.js
@@ -0,0 +1,8 @@
+const body=document.body;
+const main=document.querySelector("main");
+
+console.log(body.childNodes); /*Node List */
+console.log(body.children); /*HTML Collection */
+
+// console.log(main.childNodes); /*Node List */ text h1 text
+// console.log(main.children); /*HTML Collection */ h1
