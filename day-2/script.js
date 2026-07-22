25acd85cbe7e37dc47253cf9af4c0e070d5cff90
Arsh Mansuri <arsh.amin.may.2006@gmail.com>
2026-07-22 23:08:32 +0530

    practice on basics

--- day-2/script.js
+++ day-2/script.js
@@ -6,3 +6,14 @@ console.log(body.children); /*HTML Collection */
 
 // console.log(main.childNodes); /*Node List */ text h1 text
 // console.log(main.children); /*HTML Collection */ h1
+
+const h1=document.querySelector("h1");
+const h2=document.querySelector("h2");
+const h3=document.querySelector("h3");
+
+h1.textContent="i am changed via textContent"
+h2.innerText=" i am changed by innerText"
+
+h3.innerHTML="<i>i am innerHTML<i>" /*implemets the tags*/
+
+console.log(h1);
