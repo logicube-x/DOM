e2bd07ea33bf4b2990556314e2e99f106018a6b8
Arsh Mansuri <arsh.amin.may.2006@gmail.com>
2026-07-23 23:50:49 +0530

    made the bulb task with better approch using classlist toggle

--- projects/bulb/script.js
+++ projects/bulb/script.js
@@ -4,16 +4,27 @@ const btn=document.querySelector("button");
 let flag=true;
 
 btn.addEventListener("click",()=>{
+
+
     
-    if(flag){
-        bulb.style.backgroundColor="yellow";
-        btn.textContent="OFF"
-        flag=false;
-    }
+    // if(flag){
+    //     bulb.style.backgroundColor="yellow";
+    //     btn.textContent="OFF"
+    //     flag=false;
+    // }
    
-    else{
-        bulb.style.backgroundColor="transparent";
-        btn.textContent="ON";
-        flag=true
+    // else{
+    //     bulb.style.backgroundColor="transparent";
+    //     btn.textContent="ON";
+    //     flag=true
+    // }
+
+    if(bulb.classList.toggle("glow")){
+        btn.textContent="OFF";
     }
+    else{
+        btn.textContent="ON"
+    }
+
+    
 })
