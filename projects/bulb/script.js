e18808f357b68d630f18eb9fdc462f16eeef69b6
Arsh Mansuri <arsh.amin.may.2006@gmail.com>
2026-07-23 23:28:13 +0530

    made a simple bulb task

---
+++ projects/bulb/script.js
@@ -0,0 +1,19 @@
+const bulb=document.querySelector(".bulb");
+const btn=document.querySelector("button");
+
+let flag=true;
+
+btn.addEventListener("click",()=>{
+    
+    if(flag){
+        bulb.style.backgroundColor="yellow";
+        btn.textContent="OFF"
+        flag=false;
+    }
+   
+    else{
+        bulb.style.backgroundColor="transparent";
+        btn.textContent="ON";
+        flag=true
+    }
+})
