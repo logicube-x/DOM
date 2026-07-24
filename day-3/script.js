7618ca3168a8410c47d37ae62ac91fd1c16a5c66
Arsh Mansuri <arsh.amin.may.2006@gmail.com>
2026-07-24 17:30:41 +0530

    learnt about dataset

--- day-3/script.js
+++ day-3/script.js
@@ -1,1 +1,22 @@
-const h3
+const h3=document.querySelector("#id1")
+
+const res=h3.getAttribute("id");
+console.log(res); /**/
+
+const res1=h3.getAttribute("class");
+console.log(res1);
+
+h3.setAttribute("myAtt","200")
+
+h3.removeAttribute("myatt")
+
+// line 14 to 21 is about the dataset thing in which you can give custom attributes these are rarely used
+let temp=document.querySelector("#own");
+
+const check=temp.getAttribute("data-myid");
+console.log(check);
+
+temp.dataset.myId="999";
+
+console.log(temp.getAttribute("data-my-id"));
+
