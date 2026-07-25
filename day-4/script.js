70f1b828473188fa812c1ebda8e4b4d54957d0e2
Arsh Mansuri <arsh.amin.may.2006@gmail.com>
2026-07-25 19:56:47 +0530

    started with the events

---
+++ day-4/script.js
@@ -0,0 +1,24 @@
+const btn=document.querySelector("button");
+const div=document.querySelector("div")
+const main=document.querySelector("main")
+const body=document.querySelector("body")
+
+btn.addEventListener("click",(e)=>{
+    
+   console.log("clicked");
+})
+
+body.addEventListener("click",(e)=>{
+
+   console.log("clicked");
+})
+
+main.addEventListener("click",(e)=>{
+
+   console.log("clicked");
+})
+
+div.addEventListener("click",(e)=>{
+
+   console.log("clicked");
+})
