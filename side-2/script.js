d2273277e1494130c3f78ef255b008b00b77cd8a
Arsh Mansuri <arsh.amin.may.2006@gmail.com>
2026-07-26 12:37:03 +0530

    a simple logic of form

--- side-2/script.js
+++ side-2/script.js
@@ -1,5 +1,23 @@
 const form =document.querySelector("form");
+const inp1=document.querySelector("#name")
+const inp2=document.querySelector("#email")
 
-form.addEventListener("submit",()=>{
-    
+form.addEventListener("submit",(e)=>{
+    e.preventDefault();
+
+    // console.log(name.value);
+    // console.log(email.value);
+
+    // console.log(e.target[0].value);
+    // console.log(e.target[1].value);
+
+        let email=inp1.value;
+        let name=inp2.value;
+
+        console.log(name,email);
+
+        form.reset();
+
+    // Both the above ways are correct !..
+
 })
