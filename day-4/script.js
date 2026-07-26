a16622a0431ef617029659a2c793693e84742a45
Arsh Mansuri <arsh.amin.may.2006@gmail.com>
2026-07-26 23:07:09 +0530

    half task done (profile card)

--- day-4/script.js
+++ day-4/script.js
@@ -2,6 +2,7 @@ const inp1=document.querySelector("#inp1")
 const inp2=document.querySelector("#inp2")
 const form =document.querySelector("form")
 const cont=document.querySelector(".cont")
+const url=document.querySelector("#url")
 
 
 const users = [
@@ -53,12 +54,12 @@ form.addEventListener("submit",(e)=>{
    e.preventDefault();
    let name=inp1.value;
    let email=inp2.value;
-
+   let imageurl=url.value;
    if(name.trim()===''&&email.trim()==='') return;
 
   cont.innerHTML+=`  <div class="card">
         <div class="img-box">
-            <img src="https://images.unsplash.com/photo-1781819822600-e49d74e5bbf0?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw0fHx8ZW58MHx8fHx8" alt="">
+            <img src="${imageurl}" alt="">
         </div>
         <div id="text">
                 <h3>${name}</h3>
